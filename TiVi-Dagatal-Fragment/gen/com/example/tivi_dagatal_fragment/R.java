1080fbcd7829fbe149b9c529395770bc039fde31
Steinunn <stf7@hi.is>
2014-10-30 16:18:44 +0000

    Episodes fragment tilraun 2

--- TiVi-Dagatal-Fragment/gen/com/example/tivi_dagatal_fragment/R.java
+++ TiVi-Dagatal-Fragment/gen/com/example/tivi_dagatal_fragment/R.java
@@ -1209,12 +1209,13 @@ containing a value of this type.
         public static final int activity_main=0x7f030018;
         public static final int drawer_list_item=0x7f030019;
         public static final int fragment_cal=0x7f03001a;
-        public static final int fragment_list=0x7f03001b;
-        public static final int fragment_popular=0x7f03001c;
-        public static final int fragment_search=0x7f03001d;
-        public static final int fragment_search_results=0x7f03001e;
-        public static final int fragment_test=0x7f03001f;
-        public static final int support_simple_spinner_dropdown_item=0x7f030020;
+        public static final int fragment_episode=0x7f03001b;
+        public static final int fragment_list=0x7f03001c;
+        public static final int fragment_popular=0x7f03001d;
+        public static final int fragment_search=0x7f03001e;
+        public static final int fragment_search_results=0x7f03001f;
+        public static final int fragment_test=0x7f030020;
+        public static final int support_simple_spinner_dropdown_item=0x7f030021;
     }
     public static final class menu {
         public static final int main=0x7f0d0000;
