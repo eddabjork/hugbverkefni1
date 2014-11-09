ac211a00ad6390fe244f60dc4029c4a511f813d6
eddabjork <ebk13@hi.is>
2014-11-09 14:29:12 +0000

    vantaði Show.java

--- TiVi-Dagatal-Fragment/src/Dtos/Show.java
+++ TiVi-Dagatal-Fragment/src/Dtos/Show.java
@@ -1,11 +1,14 @@
 /**
- * Nafn:		Steinunn Friðgeirsdóttir
- * Dagsetning:	2. október 2014
- * Markmið:		Klasinn er hlutur sem geymir upplýsingar um þáttaröð.
+ * Nafn:		Steinunn Friï¿½geirsdï¿½ttir
+ * Dagsetning:	2. oktï¿½ber 2014
+ * Markmiï¿½:		Klasinn er hlutur sem geymir upplï¿½singar um ï¿½ï¿½ttarï¿½ï¿½.
  **/
 package Dtos;
 
 import java.util.List;
+
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 public class Show {
 	private String title;
@@ -27,6 +30,9 @@ public class Show {
 	private String banner;
 	private String imdbRating;
 	private List<Season> seasons;
+	private LinearLayout infoLayout;
+	private LinearLayout infoMain;
+	private ScrollView scrollView;
 	
 	public Show(){}
 	
@@ -38,115 +44,115 @@ public class Show {
 	//Getters
 	
 	//Notkun: airDay = show.getAirDay()
-	//Eftirskilyrði: airDay frumsýningardegi þáttaraðar (vikudegi).
+	//Eftirskilyrï¿½i: airDay frumsï¿½ningardegi ï¿½ï¿½ttaraï¿½ar (vikudegi).
 	public String getAirDay() {
 		return airDay;
 	}
 	
 	//Notkun: airTime = show.getAirTime()
-	//Eftirskilyrði: airTime er frumsýningartími þáttaraðar í USA.
+	//Eftirskilyrï¿½i: airTime er frumsï¿½ningartï¿½mi ï¿½ï¿½ttaraï¿½ar ï¿½ USA.
 	public String getAirTime() {
 		return airTime;
 	}
 	
 	//Notkun: url = show.getBanner()
-	//Eftirskilyrði: url er slóð á banner mynd þáttaraðar.
+	//Eftirskilyrï¿½i: url er slï¿½ï¿½ ï¿½ banner mynd ï¿½ï¿½ttaraï¿½ar.
 	public String getBanner() {
 		return banner;
 	}
 	
 	//Notkun: country = show.getCountry()
-	//Eftirskilyrði: country er upprunalandi þáttaraðar.
+	//Eftirskilyrï¿½i: country er upprunalandi ï¿½ï¿½ttaraï¿½ar.
 	public String getCountry() {
 		return country;
 	}
 	
 	//Notkun: ended = show.isEnded()
-	//Eftirskilyrði: ended er true eða false eftir því 
-	//				 hvort þáttaröð sé hætt í sýningu eða ekki.
+	//Eftirskilyrï¿½i: ended er true eï¿½a false eftir ï¿½vï¿½ 
+	//				 hvort ï¿½ï¿½ttarï¿½ï¿½ sï¿½ hï¿½tt ï¿½ sï¿½ningu eï¿½a ekki.
 	public boolean isEnded() {
 		return ended;
 	}
 	
 	//Notkun: fanart = show.getFanart()
-	//Eftirskilyrði: fanart er slóð á áðdáendalist þáttaraðar.
+	//Eftirskilyrï¿½i: fanart er slï¿½ï¿½ ï¿½ ï¿½ï¿½dï¿½endalist ï¿½ï¿½ttaraï¿½ar.
 	public String getFanart() {
 		return fanart;
 	}
 	
 	//Notkun: firstAired = show.getFirstAired()
-	//Eftirskilyrði: Skilar dagsetningunni sem þátturinn er sýndur fyrst.
+	//Eftirskilyrï¿½i: Skilar dagsetningunni sem ï¿½ï¿½tturinn er sï¿½ndur fyrst.
 	public String getFirstAired() {
 		return firstAired;
 	}
 	
 	//Notkun: genres = show.getGenres()
-	//Eftirskilyrði: genres er tegund þáttaraðar.
+	//Eftirskilyrï¿½i: genres er tegund ï¿½ï¿½ttaraï¿½ar.
 	public List<String> getGenres() {
 		return genres;
 	}
 	
 	//Notkun: imdbId = show.getImdbId()
-	//Eftirskilyrði: imdbId er IMDb auðkenni þáttaraðar.
+	//Eftirskilyrï¿½i: imdbId er IMDb auï¿½kenni ï¿½ï¿½ttaraï¿½ar.
 	public String getImdbId() {
 		return imdbId;
 	}
 	
 	//Notkun: network = show.getNetwork()
-	//Eftirskilyrði: network er stöð sem þáttaröð er sýnd á í USA.
+	//Eftirskilyrï¿½i: network er stï¿½ï¿½ sem ï¿½ï¿½ttarï¿½ï¿½ er sï¿½nd ï¿½ ï¿½ USA.
 	public String getNetwork() {
 		return network;
 	}
 	
 	//Notkun: overview = show.getOverview()
-	//Eftirskilyrði: overview er lýsing á þáttaröð.
+	//Eftirskilyrï¿½i: overview er lï¿½sing ï¿½ ï¿½ï¿½ttarï¿½ï¿½.
 	public String getOverview() {
 		return overview;
 	}
 	
 	//Notkun: poster = show.getPoster()
-	//Eftirskilyrði: poster er slóð á forsíðumynd þáttaraðar.
+	//Eftirskilyrï¿½i: poster er slï¿½ï¿½ ï¿½ forsï¿½ï¿½umynd ï¿½ï¿½ttaraï¿½ar.
 	public String getPoster() {
 		return poster;
 	}
 	
 	//Notkun: title = show.getTitle()
-	//Eftirskilyrði: title er titill þáttaraðar.
+	//Eftirskilyrï¿½i: title er titill ï¿½ï¿½ttaraï¿½ar.
 	public String getTitle() {
 		return title;
 	}
 	
 	//Notkun: tvdbId = show.getTvdbId()
-	//Eftirskilyrði: tvdbId er Tvdb auðkenni.
+	//Eftirskilyrï¿½i: tvdbId er Tvdb auï¿½kenni.
 	public String getTvdbId() {
 		return tvdbId;
 	}
 	
 	//Notkun: url = show.getUrl()
-	//Eftirskilyrði: url er slóð á þáttaröð á trakt síðunni.
+	//Eftirskilyrï¿½i: url er slï¿½ï¿½ ï¿½ ï¿½ï¿½ttarï¿½ï¿½ ï¿½ trakt sï¿½ï¿½unni.
 	public String getUrl() {
 		return url;
 	}
 	
 	//Notkun: year = show.getYear()
-	//Eftirskilyrði: year er ár sem þáttaröð kom út.
+	//Eftirskilyrï¿½i: year er ï¿½r sem ï¿½ï¿½ttarï¿½ï¿½ kom ï¿½t.
 	public String getYear() {
 		return year;
 	}
 	//Notkun: dataTitle = show.getDataTitle()
-	//Eftirskilyrði: dataTitle er auðkenni þáttaraðar fyrir vefþjónustu.
+	//Eftirskilyrï¿½i: dataTitle er auï¿½kenni ï¿½ï¿½ttaraï¿½ar fyrir vefï¿½jï¿½nustu.
 	public String getDataTitle(){
 		return dataTitle;
 	}
 	
 	//Notkun: rating = show.getImdbRating()
-	//Eftirskilyrði: rating er IMDb einkunn þáttaraðar.
+	//Eftirskilyrï¿½i: rating er IMDb einkunn ï¿½ï¿½ttaraï¿½ar.
 	public String getImdbRating(){
 		return imdbRating;
 	}
 	
 	//Notkun: show.getSeasons(seasons)
-	//Eftirskilyrði: seasons eru seríur show.
+	//Eftirskilyrï¿½i: seasons eru serï¿½ur show.
 	public List<Season> getSeasons() {
 		return seasons;
 	}
@@ -154,116 +160,140 @@ public class Show {
 	/*Setters*/
 	
 	//Notkun: show.setAirDay(airDay)
-	//Eftirskilyrði: airDay er furmsýningaradagur show.
+	//Eftirskilyrï¿½i: airDay er furmsï¿½ningaradagur show.
 	public void setAirDay(String airDay) {
 		this.airDay = airDay;
 	}
 	
 	//Notkun: show.setAirTime(airTime)
-	//Eftirskilyrði: airTime er frumsýningartími show.
+	//Eftirskilyrï¿½i: airTime er frumsï¿½ningartï¿½mi show.
 	public void setAirTime(String airTime) {
 		this.airTime = airTime;
 	}
 	
 	//Notkun: show.setBanner(banner)
-	//Eftirskilyrði: banner er url á banner mynd show.
+	//Eftirskilyrï¿½i: banner er url ï¿½ banner mynd show.
 	public void setBanner(String banner) {
 		this.banner = banner;
 	}
 
 	//Notkun: show.setCountry(country)
-	//Eftirskilyrði: country er sýningarland show.
+	//Eftirskilyrï¿½i: country er sï¿½ningarland show.
 	public void setCountry(String country) {
 		this.country = country;
 	}
 
 	//Notkun: show.setEnded(ended)
-	//Eftirskilyrði: ended er true eða false eftir því hvort show er í sýningu eða ekki.
+	//Eftirskilyrï¿½i: ended er true eï¿½a false eftir ï¿½vï¿½ hvort show er ï¿½ sï¿½ningu eï¿½a ekki.
 	public void setEnded(boolean ended) {
 		this.ended = ended;
 	}
 
 	//Notkun: show.setFanart(fanart)
-	//Eftirskilyrði: fanart er url á aðdáendalist show.
+	//Eftirskilyrï¿½i: fanart er url ï¿½ aï¿½dï¿½endalist show.
 	public void setFanart(String fanart) {
 		this.fanart = fanart;
 	}
 
 	//Notkun: show.setFirstAired(firstAired)
-	//Eftirskilyrði: firstAired frumsýningardagsetning show.
+	//Eftirskilyrï¿½i: firstAired frumsï¿½ningardagsetning show.
 	public void setFirstAired(String firstAired) {
 		this.firstAired = firstAired;
 	}
 
 	//Notkun: show.setGenres(genres)
-	//Eftirskilyrði: genres er tegund show.
+	//Eftirskilyrï¿½i: genres er tegund show.
 	public void setGenres(List<String> genres) {
 		this.genres = genres;
 	}
 
 	//Notkun: show.setImdbId(imdbId)
-	//Eftirskilyrði: imdbId er IMDb aukenni show.
+	//Eftirskilyrï¿½i: imdbId er IMDb aukenni show.
 	public void setImdbId(String imdbId) {
 		this.imdbId = imdbId;
 	}
 
 	//Notkun: show.setNetwork(network)
-	//Eftirskilyrði: network er sú stöð sem show er sýnd á.
+	//Eftirskilyrï¿½i: network er sï¿½ stï¿½ï¿½ sem show er sï¿½nd ï¿½.
 	public void setNetwork(String network) {
 		this.network = network;
 	}
 
 	//Notkun: show.setOverview(overview)
-	//Eftirskilyrði: overview er lýsing á show.
+	//Eftirskilyrï¿½i: overview er lï¿½sing ï¿½ show.
 	public void setOverview(String overview) {
 		this.overview = overview;
 	}
 
 	//Notkun: show.setPoster(poster)
-	//Eftirskilyrði: poster er url á aðalmynd show.
+	//Eftirskilyrï¿½i: poster er url ï¿½ aï¿½almynd show.
 	public void setPoster(String poster) {
 		this.poster = poster;
 	}
 	
 	//Notkun: show.setTitle(title)
-	//Eftirskilyrði: title er titill show.
+	//Eftirskilyrï¿½i: title er titill show.
 	public void setTitle(String title) {
 		this.title = title;
 	}
 
 	//Notkun: show.setTvdbId(id)
-	//Eftirskilyrði: id er Tvdb auðkenni show.
+	//Eftirskilyrï¿½i: id er Tvdb auï¿½kenni show.
 	public void setTvdbId(String tvdbId) {
 		this.tvdbId = tvdbId;
 	}
 
 	//Notkun: show.setUrl(url)
-	//Eftirskilyrði: url er slóð show á trakt.
+	//Eftirskilyrï¿½i: url er slï¿½ï¿½ show ï¿½ trakt.
 	public void setUrl(String url) {
 		this.url = url;
 	}
 
 	//Notkun: show.setYear(Year)
-	//Eftirskilyrði: year er árið sem show var gefinn út fyrst.
+	//Eftirskilyrï¿½i: year er ï¿½riï¿½ sem show var gefinn ï¿½t fyrst.
 	public void setYear(String year) {
 		this.year = year;
 	}
 
 	//Notkun: show.setDataTitle(title)
-	//Eftirskilyrði: title er aukenni show á trakt.
+	//Eftirskilyrï¿½i: title er aukenni show ï¿½ trakt.
 	public void setDataTitle(String dataTitle){
 		this.dataTitle = dataTitle;
 	}
 
 	//Notkun: show.setImdbRating(rating)
-	//Eftirskilyrði: rating er IMDb einkunn show.
+	//Eftirskilyrï¿½i: rating er IMDb einkunn show.
 	public void setImdbRating(String rating){
 		this.imdbRating = rating;
 	}
 	
 	//Notkun: show.setSeasons(seasons)
-	//Eftirskilyrði: seasons eru seríur show.
+	//Eftirskilyrï¿½i: seasons eru serï¿½ur show.
 	public void setSeasons(List<Season> seasons) {
 		this.seasons = seasons;
+	}
+	
+	public void setInfoLayout(LinearLayout infolayout) {
+		this.infoLayout = infolayout;
+	}
+	
+	public void setInfoMain(LinearLayout infomain) {
+		this.infoMain = infomain;
+	}
+	
+	public void setScrollView(ScrollView scrollview) {
+		this.scrollView = scrollview;
+	}
+	
+	public LinearLayout getInfoLayout() {
+		return this.infoLayout;
+	}
+	
+	public LinearLayout getInfoMain() {
+		return this.infoMain;
+	}
+	
+	public ScrollView getScrollView() {
+		return this.scrollView;
 	}
 }
