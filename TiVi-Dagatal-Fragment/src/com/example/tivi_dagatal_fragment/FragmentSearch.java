325b9d28709b1b3c9339c0924344e333ad28d058
Jóhanna Agnes <johannaagma@gmail.com>
2014-11-18 14:16:59 +0000

    Search-enter vonandi

--- TiVi-Dagatal-Fragment/src/com/example/tivi_dagatal_fragment/FragmentSearch.java
+++ TiVi-Dagatal-Fragment/src/com/example/tivi_dagatal_fragment/FragmentSearch.java
@@ -10,25 +10,68 @@ package com.example.tivi_dagatal_fragment;
 import Utils.VariousUtils;
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnKeyListener;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
+import android.widget.TableLayout;
+import android.widget.Toast;
 
 public class FragmentSearch extends Fragment {
 	Fragment results;
 	
 	@Override
 	//Eftir: Birtir fragment með leitarglugga og takka
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.fragment_search, container, false);
-		results = new FragmentSearchResults();
-        return rootView;
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        
+        LinearLayout mainLayout = new LinearLayout(getActivity());
+		mainLayout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+		mainLayout.setOrientation(LinearLayout.HORIZONTAL);
+		
+        setLayout(mainLayout);
+		view = mainLayout;
+        return view;
+	}
+	
+	//Notkun: setLayout();
+	//Eftir: Búið er að setja upp leitarútlitið
+	public void setLayout(LinearLayout mainLayout){
+		final EditText editText = new EditText(getActivity());
+		TableLayout.LayoutParams params = new TableLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, 1f);
+		params.setMargins(10,10,10,10);
+		editText.setLayoutParams(params);
+		editText.setId(R.id.leitarbox);
+		editText.setOnKeyListener(new OnKeyListener() {
+		    public boolean onKey(View v, int keyCode, KeyEvent event) {
+		        if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+		          searchClick(v);
+		          return true;
+		        }
+		        return false;
+		    }
+		});
+		
+		ImageButton button = new ImageButton(getActivity());
+		button.setImageResource(R.drawable.search);
+		button.setBackgroundColor(Color.TRANSPARENT);
+		button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+            	searchClick(view);
+            }
+        });
+		
+		mainLayout.addView(editText);
+		mainLayout.addView(button);
 	}
 	
 	//Notkun: searchClick(view)
