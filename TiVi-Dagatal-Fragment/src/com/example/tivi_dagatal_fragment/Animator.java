e830cd128edf8de0e65ef9d57d7d450f17a9e441
Steinunn <stf7@hi.is>
2014-11-21 13:34:10 +0000

    Animator endurskjalaður

--- TiVi-Dagatal-Fragment/src/com/example/tivi_dagatal_fragment/Animator.java
+++ TiVi-Dagatal-Fragment/src/com/example/tivi_dagatal_fragment/Animator.java
@@ -1,7 +1,7 @@
 /**
- * Nafn:	   Edda Björk Konráðsdóttir
- * Dagsetning: 30. október 2014
- * Markmið:	   Birta View fallega
+ * Nafn:	   Edda Bjork Konradsdottir
+ * Dagsetning: 30. oktober 2014
+ * Markmid:	   Birta View fallega
  */
 
 package com.example.tivi_dagatal_fragment;
@@ -30,7 +30,7 @@ public class Animator extends Animation {
     }
 
     //Notkun:		 view.applyTransformation(time, transformation)
-    //Eftirskilyrði: Búið er að birta view á tímanum time inní hlutnum t
+    //Eftirskilyrdi: Buid er ad birta view a timanum time inni hlutnum t
     protected void applyTransformation(float interpolatedTime, Transformation t) {
         super.applyTransformation(interpolatedTime, t);
         if (interpolatedTime < 1.0f) {
@@ -54,7 +54,7 @@ public class Animator extends Animation {
     }
    
     //Notkun: 		 view.setHeightForWrapContent(act, view)
-    //Eftirskilyrði: Búið er að rétta hæð á view við aðgerðina activity
+    //Eftirskilyrdi: Buid er ad setja retta haed a view vid adgerdina activity
     public static void setHeightForWrapContent(Activity activity, View view) {
         DisplayMetrics metrics = new DisplayMetrics();
         activity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
