1c223b1a9f281c3b42ac420d0006ace66db3ab51
eddabjork <ebk13@hi.is>
2014-11-21 13:46:18 +0000

    laga comment FragmentSearchResults

--- TiVi-Dagatal-Fragment/src/com/example/tivi_dagatal_fragment/FragmentSearchResults.java
+++ TiVi-Dagatal-Fragment/src/com/example/tivi_dagatal_fragment/FragmentSearchResults.java
@@ -1,8 +1,8 @@
 /**
- * Nafn: 		Steinunn Fri�geirsd�ttir
- * Dagsetning: 	30. okt�ber 2014
- * Markmi�: 	FragmentSearchResults er fragment sem birtir lista
- * 				af ��ttum sem notandi leitar eftir.
+ * Nafn: 		Steinunn Fridgeirsdottir
+ * Dagsetning: 	30. oktober 2014
+ * Markmid: 	FragmentSearchResults er fragment sem birtir lista
+ * 				af thattum sem notandi leitar eftir.
  */
 package com.example.tivi_dagatal_fragment;
 
@@ -32,7 +32,7 @@ public class FragmentSearchResults extends Fragment{
 	private List<String> open = new ArrayList<String>();
 	
 	@Override
-	//Eftir: birtir fragmenti� me� leitarni�urst��unum
+	//Eftir: birtir fragmentid med leitarnidurstodunum
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fragment_search_results, container, false);
 		
@@ -53,22 +53,22 @@ public class FragmentSearchResults extends Fragment{
     }
 	
 	//Notkun: onAttach(activity)
-	//Eftir:  b�i� er a� tengja gagnagrunninn vi� fragmenti�
+	//Eftir:  buid er ad tengja gagnagrunninn vid fragmentid
 	public void onAttach(Activity activity) {
         super.onAttach(activity);
         dbHelper = new DbUtils(activity);
     }
 
 	/**
-     * Nafn: 		Steinunn Fri�geirsd�ttir
-     * Dagsetning: 	30. okt�ber 2014
-     * Markmi�: 	Framkv�mir �r��avinnu til a� birta leitarni�urst��ur
-     * 				fr� vef�j�nustu � fargmenti. 
+     * Nafn: 		Steinunn Fridgeirsdottir
+     * Dagsetning: 	30. oktober 2014
+     * Markmid: 	Framkvaemir thradavinnu til ad birta leitarnidurstodur
+     * 				fra vefthjonustu i fragmenti. 
      */   	
 	private class SearchShowsTask extends AsyncTask<String, Integer, List<Show>> {
 		//Notkun: 	doInBackground(queries)
-		//Eftir:	B�i� er a� b�a til vef�j�nustu og s�kja �� ��tti sem 
-		//			notandi leita�i eftir � bakgrunns��r�i og skila �eim.
+		//Eftir:	Buid er ad bua til vefthjonustu og saekja tha thaetti sem 
+		//			notandi leitadi eftir i bakgrunnsthraedi og skila theim.
 		protected List<Show> doInBackground(String... queries) {         
 			TraktClient search = new TraktClient();	    	 
 			List<Show> searchShows = search.searchShow(queries[0]);  
@@ -76,16 +76,16 @@ public class FragmentSearchResults extends Fragment{
 		}
 		
 		// Notkun: onPreExecute()
-		// Eftir:  progressDialog hefur veri� stillt sem � a� s�na � me�an notandi er a� b��a
+		// Eftir:  progressDialog hefur verid stillt sem a ad syna a medan notandi er ad bida
 		protected void onPreExecute() {  
     		progressDialog = LayoutUtils.showProgressDialog(R.string.list_process_title, 
     				R.string.search_process_msg, getActivity());
         } 
 		
 		//Notkun:	onPostExecute(searchShows)
-		//Eftir: 	B�i� er a� taka searchShows listann og birta hann.
-		//          � listanum eru l�ka takkar sem h�gt er a� �ta � og 
-		// 	        �� b�tist ��ttur � gagnagrunn. 
+		//Eftir: 	Buid er ad taka searchShows listann og birta hann.
+		//          I listanum eru lika takkar sem haegt er ad yta a og 
+		// 	        tha baetist thattur i gagnagrunn. 
 		protected void onPostExecute(List<Show> searchShows) {
 			LinearLayout listLayout = LayoutUtils.getRegListLayout(searchShows, getActivity(), dbHelper, open);
 			scrollView.addView(listLayout);
